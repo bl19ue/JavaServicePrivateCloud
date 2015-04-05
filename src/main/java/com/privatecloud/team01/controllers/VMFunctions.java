f42e686f4092ebeca76702de986fc84f1898a861
Sumit Valecha <svalecha91@gmail.com>
2015-04-05 15:35:47 -0700

    first commit

--- src/main/java/com/privatecloud/team01/controllers/VMFunctions.java
+++ src/main/java/com/privatecloud/team01/controllers/VMFunctions.java
@@ -1,10 +1,11 @@
 package com.privatecloud.team01.controllers;
 
 import com.privatecloud.team01.models.VMStats;
+import com.vmware.vim25.mo.VirtualMachine;
 
 public class VMFunctions {
 	public boolean createVM(){
-		
+	
 	}
 	
 	public boolean startVM(){
