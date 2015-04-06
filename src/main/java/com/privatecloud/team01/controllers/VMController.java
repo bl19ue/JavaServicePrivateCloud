6afa0abb77188eece6058139175e3a5b0cb3058b
Prajakta Naik <pjnaik.1990@gmail.com>
2015-04-05 20:51:53 -0700

    Let us See

--- src/main/java/com/privatecloud/team01/controllers/VMController.java
+++ src/main/java/com/privatecloud/team01/controllers/VMController.java
@@ -6,14 +6,81 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.*;
 
+import com.privatecloud.team01.models.VM;
+
 import java.util.*;
 
 @RequestMapping("/")
 @RestController
 public class VMController {
-	@RequestMapping(value="/vm/{id}", method=RequestMethod.GET)
+	VMFunctions vmfunctions = new VMFunctions();
+	//view the VM, user passes the uuid
+	@RequestMapping(value="/vm/{uuid}", method=RequestMethod.GET)
     @ResponseBody
-    public String getVM(@PathVariable("id") int id){
-    	return "You have got the VM " + id;
+    public String getVM(@PathVariable("uuid") int uuid){
+    	return "You have got the VM " + uuid;
     }
+	
+	//start the vm, after the user gives the uuid
+	@RequestMapping(value="/vm/{uuid}", method=RequestMethod.GET)
+    @ResponseBody
+    public String startVM(@PathVariable("id") String uuid) throws Exception{
+		boolean vm_status=vmfunctions.VMoperations(uuid, "poweron");
+		if(vm_status==true)
+    	return "You have started the VM " + uuid;
+		else 
+			return "You have started the VM " + uuid;
+    }
+	
+	//reboot the vm, after the user gives the uuid
+		@RequestMapping(value="/vm/{uuid}", method=RequestMethod.GET)
+	    @ResponseBody
+	    public String reboottVM(@PathVariable("id") String uuid) throws Exception{
+			boolean vm_status=vmfunctions.VMoperations(uuid, "reboot");
+			if(vm_status==true)
+	    	return "You have started the VM " + uuid;
+			else 
+				return "You have started the VM " + uuid;
+	    }
+		
+		//reset the vm, after the user gives the uuid
+		@RequestMapping(value="/vm/{uuid}", method=RequestMethod.GET)
+	    @ResponseBody
+	    public String resetVM(@PathVariable("id") String uuid) throws Exception{
+			boolean vm_status=vmfunctions.VMoperations(uuid, "reset");
+			if(vm_status==true)
+	    	return "You have started the VM " + uuid;
+			else 
+				return "You have started the VM " + uuid;
+	    }
+		
+		//standby the vm, after the user gives the uuid
+				@RequestMapping(value="/vm/{uuid}", method=RequestMethod.GET)
+			    @ResponseBody
+			    public String standbyVM(@PathVariable("id") String uuid) throws Exception{
+					boolean vm_status=vmfunctions.VMoperations(uuid, "reset");
+					if(vm_status==true)
+			    	return "You have standby the VM " + uuid;
+					else 
+						return "You have standby the VM " + uuid;
+			    }
+	// stop the vm
+	@RequestMapping(value="/vm/{uuid}", method=RequestMethod.GET)
+    @ResponseBody
+    public String stopVM(@PathVariable("uuid") String uuid) throws Exception{
+		boolean vm_status=vmfunctions.VMoperations(uuid,"poweroff");
+		if(vm_status==true)
+    	return "You have stopped the VM " + uuid;
+		else
+			return "VM cannot be stopped " + uuid;
+			
+    }
+	
+	 @RequestMapping(value = "/vm/{template_id}",method = RequestMethod.POST)
+	  public String createVM(@RequestBody VM vm,@PathVariable("template_id") String template_id) throws Exception{
+		 vmfunctions.createVM(vm.getVmName(),template_id);
+        return "";
+
+	  }
+
 }
--- src/main/java/com/privatecloud/team01/controllers/VMFunctions.java
+++ src/main/java/com/privatecloud/team01/controllers/VMFunctions.java
@@ -1,27 +1,152 @@
 package com.privatecloud.team01.controllers;
 
+import java.net.URL;
+import java.rmi.RemoteException;
+
 import com.privatecloud.team01.models.VMStats;
+import com.vmware.vim25.mo.ServiceInstance;
 import com.vmware.vim25.mo.VirtualMachine;
+import com.vmware.vim25.*;
+import com.vmware.vim25.mo.*;
 
 public class VMFunctions {
-	public boolean createVM(){
-	
-	}
-	
-	public boolean startVM(){
-		
+	public boolean createVM(String newVmname, String template_id) throws Exception {
+		ServiceInstance si = new ServiceInstance(new URL(
+				"https://130.65.132.101/sdk"), "administrator", "12!@qwQW",
+				true);                    //create instance of ServiceInstance
+		Folder rootFolder = si.getRootFolder();
+	    VirtualMachine vm = (VirtualMachine) new InventoryNavigator(
+	        rootFolder).searchManagedEntity("VirtualMachine", newVmname);
+	    
+	    if(vm==null)                            //check whether there is vm or not
+	    {
+	      System.out.println("No VM " + newVmname + " found");
+	      si.getServerConnection().logout();
+	      return false;
+	    }
+	    
+	    /* if(vm.getConfig().template==true){                  //check if the vm is a template or not
+	    	System.out.println("yes Template found.");*/
+	    
+	    VirtualMachineCloneSpec cloneSpec =     new VirtualMachineCloneSpec(); //create an instance of vm clone specifications
+	    cloneSpec.setLocation(new VirtualMachineRelocateSpec());   //set location ,power and template values
+	    cloneSpec.setPowerOn(true);
+	    cloneSpec.setTemplate(false);
+
+	    Task task = vm.cloneVM_Task((Folder) vm.getParent(), template_id, cloneSpec); 
+	    
+	    System.out.println("Launching the VM creation task. " +	"Please wait ...");
+
+	    String status = task.waitForTask();   
+	    if(status==Task.SUCCESS)                //task status
+	    {
+	      System.out.println("VM got created successfully from template.");
+	    }
+	    else
+	    {
+	      System.out.println("Failure -: VM cannot be created");
+	      return false;
+	    }
+	  
+/*else
+
+	System.out.println("There is no template");
+
+	}*/
+		return true;
 	}
 
-	public boolean stopVM(){
-		
+	public boolean startVM() {
+		return false;
 	}
-	
-	public boolean getVMList(){
-		
+
+	public boolean VMoperations(String uuid, String op) throws Exception {
+
+		String vmname = "";
+		ServiceInstance si = new ServiceInstance(new URL(
+				"https://130.65.132.101/sdk"), "administrator", "12!@qwQW",
+				true);
+
+		Folder rootFolder = si.getRootFolder();
+		System.out.println(rootFolder.getName()); // Datacenters
+		ManagedEntity[] mes = rootFolder.getChildEntity();
+
+		for (int i = 0; i < mes.length; i++) {
+			System.out.println(mes[i].getName()); // T01-DC
+			if (mes[i] instanceof Datacenter) {
+				Datacenter dc = (Datacenter) mes[i];
+				Folder vmFolder = dc.getVmFolder(); // vm folder
+				System.out.println(vmFolder.getName()); // vm
+				ManagedEntity vm_mainfolder = vmFolder.getChildEntity()[0]; // Discovered virtual machine
+																			
+				ManagedEntity[] vms = new InventoryNavigator(vm_mainfolder)
+						.searchManagedEntities("VirtualMachine"); // find all vm in the discoverd folder
+																	
+				for (int j = 0; j < vms.length; j++) {
+					System.out.println(vms[j].getName());
+					if (vms[j] instanceof VirtualMachine) {
+						VirtualMachine vm = (VirtualMachine) vms[j];
+						String instanceUuid = vm.getConfig().instanceUuid;
+						System.out.println((vm.getName()) + "," + instanceUuid);
+						VirtualMachineSummary summary = (VirtualMachineSummary) (vm
+								.getSummary());
+						System.out.println(summary.toString());
+						VirtualMachineRuntimeInfo vmri = (VirtualMachineRuntimeInfo) vm
+								.getRuntime();
+						if (uuid.equals(instanceUuid)) {
+							if (vmri.getPowerState() == VirtualMachinePowerState.poweredOn
+									&& op.equals("poweroff")) {
+								Task task = vm.powerOffVM_Task();
+								task.waitForMe();
+								System.out.println("vm:" + vm.getName()
+										+ " powered off.");
+							} else if ("poweron".equalsIgnoreCase(op)) {
+								Task task = vm.powerOnVM_Task(null);
+								if (task.waitForMe() == Task.SUCCESS) {
+									System.out.println(vmname + " powered on");
+								}
+							} else if ("reboot".equalsIgnoreCase(op)) {
+								vm.rebootGuest();
+								System.out.println(vmname+ " guest OS rebooted");
+							} else if ("reset".equalsIgnoreCase(op)) {
+								Task task = vm.resetVM_Task();
+								if (task.waitForMe() == Task.SUCCESS) {
+									System.out.println(vmname + " reset");
+								}
+							} else if ("standby".equalsIgnoreCase(op)) {
+								vm.standbyGuest();
+								System.out
+										.println(vmname + " guest OS stoodby");
+							} else if ("suspend".equalsIgnoreCase(op)) {
+								Task task = vm.suspendVM_Task();
+								if (task.waitForMe() == Task.SUCCESS) {
+									System.out.println(vmname + " suspended");
+								}
+							} else if ("shutdown".equalsIgnoreCase(op)) {
+								Task task = vm.suspendVM_Task();
+								if (task.waitForMe() == Task.SUCCESS) {
+									System.out.println(vmname + " suspended");
+								}
+							} else {
+								System.out
+										.println("Invalid operation. Exiting...");
+							}
+							si.getServerConnection().logout();
+						}
+					}
+				}
+			}
+		}
+
+			return false;
 	}
-	
-	public VMStats getVMStats(){
-		
+
+	public boolean getVMList() {
+		return false;
 	}
-	
+
+	public VMStats getVMStats() {
+		return new VMStats();
+	}
+
 }
